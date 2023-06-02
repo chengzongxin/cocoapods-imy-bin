f1d0db7d33cc16bb66d5c9b0e802992febca9553
Joe.cheng <joe.cheng@corp.to8to.com>
2023-06-02 18:41:06 +0800

    【新增】1.0.1. 去掉pod install，，pod签名

--- lib/cocoapods-imy-bin/gem_version.rb
+++ lib/cocoapods-imy-bin/gem_version.rb
@@ -1,6 +1,6 @@
 
 module CBin
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
 
 module Pod
--- lib/cocoapods-imy-bin/native/installer.rb
+++ lib/cocoapods-imy-bin/native/installer.rb
@@ -115,6 +115,20 @@ module Pod
     end
   end
 
+  module Hook
+    Pod::HooksManager.register('cocoapods-imy-bin', :post_install) do |context|
+      context.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+
+          # disable code signing for pods
+          config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ""
+          config.build_settings['CODE_SIGNING_REQUIRED'] = "NO"
+          config.build_settings['CODE_SIGNING_ALLOWED'] = "NO"
+        end
+      end
+    end
+  end
+
   module Downloader
     class Cache
       # 多线程锁
